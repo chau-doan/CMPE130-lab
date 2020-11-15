2175ac79794af82048937610cdf80c4350cc79d1
chaudoan <chau.doan@sjsu.edu>
2020-11-14 23:06:32 -0800

    Initial commit:

--- lib/hw3/search_trees.py
+++ lib/hw3/search_trees.py
@@ -1,4 +1,3 @@
-
 import time
 import random
 
@@ -16,11 +15,21 @@ class Array_Search:
         for num in self.array:
             if num == key:
                 return idx
-            idx = idx+1
+            idx = idx + 1
         return False
 
     def bsearch(self, val):
-
+        low = 0
+        high = len(self.array) - 1
+        mid = 0
+        while low <= high:
+            mid = (high + low) // 2
+            if self.array[mid] < val:
+                low = mid + 1
+            elif self.array[mid] > val:
+                high = mid - 1
+            else:
+                return mid
         return False
 
 
@@ -45,22 +54,63 @@ class BST:
             self.insertNode(self.root, val)
 
     def insertNode(self, current, val):
-
-        return False
+        if current.val == val:
+            current.val = val
+        elif current.val > val:
+            if current.left is None:
+                current.left = BST_Node(val)
+            else:
+                self.insertNode(current.left, val)
+        elif current.val < val:
+            if current.right is None:
+                current.right = BST_Node(val)
+            else:
+                self.insertNode(current.right, val)
 
     def bsearch(self, val):
-
-
-        return False
+        if self.root is None:
+            return False
+        else:
+            return self.searchNode(self.root, val)
 
     def searchNode(self, current, val):
-
-        return False
+        if current is None:
+            return False
+        elif current.val == val:
+            return current
+        elif current.val > val:
+            return self.searchNode(current.left, val)
+        else:
+            return self.searchNode(current.right, val)
 
     def delete(self, val):
+        if self.bsearch(val) == False:
+            return False
+        else:
+            current = self.bsearch(val)
+            if current.left is None:
+                temp = current
+                current = current.right
+                temp = None
+                return True
+            elif current.right is None:
+                temp = current
+                current = current.left
+                temp = None
+                return True
+            else:
+                temp = self.minValueNode(current.right)
+                self.delete(temp)
+                current.val = temp
+                return True
 
-        return False
+    def minValueNode(self, current):
 
+        # loop down to find the leftmost leaf
+        while (current.left is not None):
+            current = current.left
+
+        return current.val
 
 
 class RBBST_Node:
@@ -83,24 +133,30 @@ class RBBST:
         self.root = RBBST_Node(val, color)
 
     def is_red(self, current):
-
-
-
-        return False
+        if current is None:
+            return False
+        return current.color == RED
 
     def rotate_left(self, current):
-
-
-
-        return False
+        temp = current.right
+        current.right = temp.left
+        temp.left = current
+        temp.color = current.color
+        current.color = RED
+        return temp
 
     def rotate_right(self, current):
-
-        return False
+        temp = current.left
+        current.left = temp.right
+        temp.right = current
+        temp.color = current.color
+        current.color = RED
+        return temp
 
     def flip_colors(self, current):
-
-        return False
+        current.color = RED
+        current.left.color = BLACK
+        current.right.color = BLACK
 
     def insert(self, val):
         if (self.root is None):
@@ -109,22 +165,46 @@ class RBBST:
             self.insertNode(self.root, val)
 
     def insertNode(self, current, val):
-
-
-
-
-        return False
+        if current is None:
+            return RBBST_Node(val, RED)
+        if current.val > val:
+            current.left = self.insertNode(current.left, val)
+        elif current.val < val:
+            current.right = self.insertNode(current.right, val)
+        else:
+            current.val = val
+        if self.is_red(current.right) and not self.is_red(current.left):
+            if current == self.root:
+                self.root = self.rotate_left(current)
+            else:
+                current = self.rotate_left(current)
+        elif self.is_red(current.left) and self.is_red(current.left.left):
+            if current == self.root:
+                self.root = self.rotate_left(current)
+            else:
+                current = self.rotate_right(current)
+        if self.is_red(current.left) and self.is_red(current.right):
+            self.flip_colors(current)
+        return current
 
     def bsearch(self, val):
-
-        return False
+        if self.root is None:
+            return False
+        else:
+            return self.searchNode(self.root, val)
 
     def searchNode(self, current, val):
+        if current is None:
+            return False
+        elif current.val == val:
+            return current
+        elif current.val > val:
+            return self.searchNode(current.left, val)
+        elif current.val < val:
+            return self.searchNode(current.right, val)
 
-        return False
 
 if __name__ == "__main__":
-
 
     set_sz = 10
     tut = BST()
@@ -132,17 +212,15 @@ if __name__ == "__main__":
     vals = random.sample(range(1, 100), set_sz)
 
     for idx in range(set_sz - 1):
-
         tut.insert(vals[idx])
 
-    print (tut.bsearch(vals[1]))
+    print(tut.bsearch(vals[1]))
     print(tut.bsearch(11))
 
     tut_rb = RBBST()
 
     for idx in range(set_sz - 1):
-
         tut_rb.insert(vals[idx])
 
-    print (tut.bsearch(vals[1]))
+    print(tut.bsearch(vals[1]))
     print(tut.bsearch(11))
--- test/test_hw3.py
+++ test/test_hw3.py
@@ -34,6 +34,53 @@ class Test_Search(object):
 
         assert expected == actual
 
+    def test_array_search(self):
+        vals = [1, 20, 32, 45, 46, 56, 58, 68, 90]
+        to_test = Array_Search(vals)
+        key = vals[5]
+        not_found = False
+        assert 5 == to_test.bsearch(key)
+        assert not_found == to_test.bsearch(100)
+        key = vals[6]
+        assert 6 == to_test.bsearch(key)
+
+    def test_BST(self):
+        tree = BST()
+        to_insert = [10, 20, 30, 5, 6, 15, 25, 35, 3, 8, 4, 1]
+        for i in to_insert:
+            tree.insert(i)
+
+        not_found = False
+        deleted = True
+
+        assert tree.bsearch(5).val == 5
+        assert not_found == tree.bsearch(100)
+        assert not_found == tree.bsearch(34)
+        assert not_found == tree.bsearch(26)
+        assert deleted == tree.delete(5)
+        assert not_found == tree.bsearch(5)
+        assert tree.bsearch(3).val == 3
+        assert not_found == tree.delete(100)
+        assert deleted == tree.delete(10)
+        assert tree.bsearch(30).val == 30
+
+    def test_rbtree(self):
+        tree = RBBST()
+        to_insert = [10, 20, 30, 5, 6, 15, 25, 35, 3, 8, 4, 1]
+        for i in to_insert:
+            tree.insert(i)
+        red = True
+        not_found = False
+        assert 30 == tree.bsearch(30).val
+        assert 5 == tree.bsearch(5).val
+        assert red != tree.is_red(tree.bsearch(5))
+        assert red == tree.is_red(tree.bsearch(1))
+        assert red != tree.is_red(tree.bsearch(15))
+        assert 25 == tree.bsearch(25).val
+        assert not_found == tree.bsearch(2)
+        assert not_found == tree.bsearch(12)
+
+
 
     def test_rbbst_0(self):
 
@@ -50,7 +97,7 @@ class Test_Search(object):
         key = vals[9]
         expected = vals.index(key)
 
-        actual = op_under_test.bsearch(key)
+        actual = op_under_test.bsearch(key).val
 
         assert expected == actual
 
